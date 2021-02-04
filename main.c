3e8745813a77f9e1c9f6da5ffc59779d21d4fb18
Vlad Abadzhiev <TheCryru@gmail.com>
2021-02-04 12:53:07 +0200

    Indentation

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 
 void hiFromProxyFunc() // Called by C# through a function pointer.
 {
-	printf("Hi from C# through C!\n");
+    printf("Hi from C# through C!\n");
 }
 
 typedef void (*hiFromProxyFuncPtrType)();
 extern __stdcall int Hi(hiFromProxyFuncPtrType); // Declared in Program.cs
 
-int main() {
-	Hi(&hiFromProxyFunc);
-	printf("Hi from C!\n");
-	return 0;
+int main()
+{
+    Hi(&hiFromProxyFunc);
+    printf("Hi from C!\n");
+    return 0;
 }
